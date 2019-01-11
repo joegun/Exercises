4ea6215159afe3b54f757da72b12481fbe2a9feb
JoeGun <joshgun83@yahoo.com>
2019-01-10 22:41:25 -0800

    Add project files.

---
+++ FirstProblem/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FirstProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SumOfMultiplesOfThreeOrFive(1000));
+        }
+
+        private static int SumOfMultiplesOfThreeOrFive(int n)
+        {
+            var total = 0;
+            for (var i = 1; i < n; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    total += i;
+                }
+            }
+            return total;
+        }
+    }
+}
